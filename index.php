db2fe569c2f9ea124e5aaa8c5f36a6e01b12b279
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-12-16 23:31:35 +0100

    Creating most variables

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    $to = "user_email@example.com";
+    $subject = "Subject: ";
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n\n";
+    $email_message .= "Message:\n$message";
+    $mailSent = mail($to, $subject, $message);
+}
